c4709082eb45fc0fafa270b9fe6a63a233a93741
carlos_salazar <c.salazar@nfddata.com>
2023-04-05 12:09:33 -0400

    push without bd

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { User, UserSchema } from './schema/user.schema';
+// import { User, UserSchema } from './schema/user.schema';
 import { UsersController } from './users.controller';
 import { UserService } from './users.service';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    // MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
     ConfigModule,
   ],
   controllers: [UsersController],
